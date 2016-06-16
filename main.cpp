77d01c12f945c2024a50044c50a68491f39ee638
Fluck Michael <michael.fluck@ntb.ch>
2016-06-16 11:59:51 +0000

    Startverion Video Modul

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    cout << "Kacke" << endl;
 
     return a.exec();
 }
